f630e6d60e26e9db3607d4d8121dae36a762cb6a
DESKTOP-8NADJPO\yb010 <yb010626@gmail.com>
2021-09-09 13:21:31 +0900

    [김유빈] -최초 파일 추가

---
+++ src/test/java/com/starkyb/memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starkyb.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
